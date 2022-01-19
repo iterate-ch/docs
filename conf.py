d2c0639cbff4f7b887537cb89ea11a23d90b5ddb
David Kocher <dkocher@iterate.ch>
2022-01-19 15:42:22 +0100

    Add footer credits.

--- conf.py
+++ conf.py
@@ -67,7 +67,7 @@ html_context = {
 	"conf_py_path": "/"
 }
 html_show_sourcelink = False
-html_show_sphinx = False
+html_show_sphinx = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
